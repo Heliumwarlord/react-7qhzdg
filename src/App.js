7d01b4401195acb8db6d5ce22916432fe68186e5
Heliumwarlord <125116980+Heliumwarlord@users.noreply.github.com>
2023-07-19 23:27:12 +0530

    1 file modified, 1 added

--- src/App.js
+++ src/App.js
@@ -2,16 +2,9 @@ import React,{useState} from "react";
 import "./style.css";
 
 export default function App() {
-  const [color,setcolor] = useState("Blue")
-
-  function Slave(){
-    console.log("Slave indeed");
-    
-  }
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
-    <button onClick={Slave}>Kilck</button>
       <p>Start editing to see some magic happen :)</p>
     </div>
   );
