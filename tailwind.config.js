5eb832372ff5eff31d45e5c2f09151937c44dee1
Kanagwa <roadr3461@gmail.com>
2025-04-14 21:59:54 +0530

    done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,8 @@
-module.exports = {
-  darkMode: "class", // Enable class-based dark mode
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}", // Include all JS/JSX/TS/TSX files in the src folder
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+export const darkMode = "class";
+export const content = [
+  "./src/**/*.{js,jsx,ts,tsx}", // Include all JS/JSX/TS/TSX files in the src folder
+];
+export const theme = {
+  extend: {},
 };
+export const plugins = [];
